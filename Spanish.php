1a926d186201f6bd6f0b306ec5302ec17a3a5532
javian1 <javian235@gmail.com>
2016-10-26 01:48:11 +0200

    Create Spanish.php

---
+++ Spanish.php
@@ -0,0 +1,44 @@
+<?php
+/////////////////////////////////
+// Lenguaje usado directamente en HTML
+/////////////////////////////////
+$lang_1_day					      = '1 Dia';
+$lang_1_week					= '1 Semana';
+$lang_1_month					= '1 Mes';
+$lang_video_convert_pass			= 'Paso 1 de 2. Convirtiendo';
+
+$lang_about_me					= 'Sobre mi';
+$lang_about_us					= 'Sobre nosotros';
+$lang_account_login                   	= 'Cuenta Inicio Sesion';
+$lang_action                              = 'Accion';
+$lang_active                              = 'Activar';
+$lang_activate                            = 'Activado';
+$lang_accept_invite                    	= 'Aceptar Invitacion';
+$lang_access_forbidden                    = 'Accesso Prohibido!';
+$lang_add_delete_group_videos        	= 'A�adir/Eliminar Mis Videos al Grupo';
+$lang_add_playlist                        = 'A�adir a Playlist';
+$lang_add_video                           = 'A�adir Video';
+$lang_word_add                            = 'A�adir';
+$lang_favourites                          = 'A�adir a Favoritos';
+$lang_add_audio_to_blog                   = 'A�adir Audio a Tu blog';
+$lang_add_photo_to_blog             	= 'A�adir Foto a Tu Blog';
+$lang_add_video_to_blog             	= 'A�adir Video a Tu Blog';
+$lang_added                               = 'A�adido';
+$lang_add_friends					= 'A�adir a Amigos';
+$lang_admin						= 'Administrador';
+$lang_admin_details                       = 'Detalles de Administrador';
+$lang_advertise                           = 'Anuncio';
+$lang_ads                                 = 'Ads';
+$lang_age                                 = 'Edad';
+$lang_album		    				= 'Album';
+$lang_album_source				= 'Album de Musica es de';
+$lang_word_albums					= 'Albums';
+$lang_album_settings				= 'Opciones de Album';
+$lang_album_type                		= 'Tipo de Album';
+$lang_albums_used              		= 'Albums Usados';
+$lang_all                                 = 'Todo';
+$lang_all_uploads_approval   			= 'Todos los archivos multimedia subidos pueden estar sujetos a aprobacion del personal';
+$lang_all_groups                    	= 'Todos los Grupos';
+$lang_all_members                         = 'Todos los Miembros';
+$lang_allow_comm                          = 'Permitir Comentarios';
+$lang_allow_embed 
